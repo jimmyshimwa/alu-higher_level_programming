7accaee0d984590f04a2bffd7a1a61c946f6591a
jimmy shimwa <p.shimwa@alustudent.com>
2026-06-27 12:10:51 -0700

    Fix task 6 and 8 with correct approach

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-number = 42
-print(word[:number])
-number = 0
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[105:111] + str[0:6]
+print(str)
